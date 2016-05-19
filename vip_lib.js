8215da099525aedc6fefc1d93991bde6e555a68f
ct <ct6787@gmail.com>
2016-05-19 14:16:24 +0100

    dev

--- vip_lib.js
+++ vip_lib.js
@@ -188,6 +188,7 @@ VipHost.prototype.createMultiCol = function()
 VipHost.prototype.scale_font = function()
 {
 	this.div.style.fontSize = fmt("^px", Math.floor(0.7*vip.cell.height));
+	this.div.style.fontSize = "11px";
 
 	var a = document.createElement('div');
 	a.innerHTML = "a";
