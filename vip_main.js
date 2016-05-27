a502b119b2f44426f2c0a7ed0005630af01b93bd
ct <ct6787@gmail.com>
2016-05-27 16:04:19 +0100

    dev

--- vip_main.js
+++ vip_main.js
@@ -76,10 +76,6 @@ function VipInit()
 	}
 	else
 		InitSingleColView();
-
-	document.addEventListener('DOMSubtreeModified', function () {console.log('DOMSubtreeModified')}, false);
-	document.addEventListener('transitionend', function () {console.log('transitionend')}, false);
-	document.addEventListener('message', function () {console.log('message')}, false);
 }
 
 function InitSingleColView()
