eef36fc43aa04c4df819e6efddfab4eba0502b18
ct <ct6787@gmail.com>
2016-12-20 01:46:30 +0000

    dev

--- vip_lib.js
+++ vip_lib.js
@@ -302,7 +302,7 @@ VipCol.prototype.addMonthHeader = function(vdt_month)
 	hdr.setAttribute('onclick', "onclick_month_header(event);");
 	hdr.style.width = vip.cell.width;
 	hdr.style.textAlign = "center";
-	hdr.style.verticalAlign = "text-bottom";
+	hdr.style.verticalAlign = "bottom";
 	hdr.style.pointerEvents = "all";
 	hdr.style.cursor = "pointer";
 }
