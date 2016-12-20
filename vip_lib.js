cd91e685c4c011f3ea5003993697ca456b01be98
ct <ct6787@gmail.com>
2016-12-20 19:52:22 +0000

    dev

--- vip_lib.js
+++ vip_lib.js
@@ -712,7 +712,7 @@ function VipSingleDayEvent(vipcell, event)
 		this.viptitle = new VipDiv(this, "viptitle");
 		this.viptitle.div.style.whiteSpace = "nowrap";
 		this.viptitle.div.style.lineHeight = fmt("^px", vip.cell.height);
-		this.viptitle.setPos(x_off, 0);
+		this.viptitle.setPos(x_off, 1);
 
 		if (vip.events.title.colour)
 			this.viptitle.div.style.color = event.palette.medium;
