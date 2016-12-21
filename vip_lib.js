d0f3574d47288a34b246dd3ad7c835d6c73dc3f2
ct <ct6787@gmail.com>
2016-12-21 21:59:35 +0000

    dev

--- vip_lib.js
+++ vip_lib.js
@@ -151,10 +151,10 @@ VipHost.prototype.createMultiCol = function()
 		vip.multi_col.count = (vip.multi_col.count / 2);
 
 	// scale dimensions depending on available space
-	vip.cell.width = Math.max(Math.floor(document.body.clientWidth/vip.multi_col.count), 5);
-	vip.cell.height = Math.max(Math.floor(document.body.clientHeight/(31+6+1)), 10);  // max days + max offset + month name
-	vip.events.marker.width = Math.floor(0.037 * vip.cell.width);
-	vip.events.marker.height = Math.floor(0.55 * vip.cell.height);
+	vip.cell.width = Math.floor(document.body.clientWidth/vip.multi_col.count);
+	vip.cell.height = Math.floor(document.body.clientHeight/(31+6+1));  // max days + max offset + month name
+	vip.events.marker.width = Math.max(Math.floor(0.037 * vip.cell.width), 5);
+	vip.events.marker.height = Math.max(Math.floor(0.55 * vip.cell.height), 10);
 	this.div.style.fontSize = fmt("^px", vip.events.marker.height);
 	vip.cell.margin = vip.cell.height+4;
 
