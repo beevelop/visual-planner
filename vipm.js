e00ce4b16e8e4636459fbdfcb3fccd8a97af9d7a
ct <ct6787@gmail.com>
2017-01-22 00:14:18 +0000

    dev

--- vipm.js
+++ vipm.js
@@ -7,10 +7,9 @@ function vip_init()
 
 	var banner = document.getElementById("banner");
 	banner.style.margin = "3px 0px";
-	banner.style.paddingLeft = "10px";
 	banner.style.backgroundColor = "#DAE4EB";
 	banner.style.lineHeight = "2.5em";
-	banner.style.fontSize = "1.2em";
+	banner.style.fontSize = "1em";
 
 	var grid = document.getElementById("grid");
 	grid.style.height = "100%";
@@ -19,13 +18,23 @@ function vip_init()
 	grid.scrolling = "no";
 	grid.onload = onGridLoad;
 	grid.src="vip_grid.html";
+
+	var so = "n/a";
+	if (screen.orientation)
+	if (screen.orientation.type)
+		so = screen.orientation.type;
+	if (screen.msOrientation)  // edge, ie
+		so = screen.msOrientation;
+	if (so.includes("portrait"))
+		document.body.style.fontSize = "1.2em";
 }
 
 function onGridLoad()
 {
 	var ivipgrid = document.getElementById("grid").contentWindow.IVipGrid;
 
-	//ivipgrid.layout.col_count = 10;
-	ivipgrid.layout.font_scale = 1;
+	ivipgrid.layout.col_count = 8;
+	ivipgrid.layout.font_scale = 0.8;
+
 	ivipgrid.create();
 }
