c922414b052b4948a0785484bc7e9fb5ced959fb
ct <ct6787@gmail.com>
2017-01-23 20:42:08 +0000

    dev

--- vip.js
+++ vip.js
@@ -1,31 +1,11 @@
 function vip_init()
 {
-	document.body.style.margin = "0px";
-	document.body.style.height = "100vh";
-	document.body.style.display = "flex";
-	document.body.style.flexDirection = "column";
-
-	var banner = document.getElementById("banner");
-	banner.style.margin = "3px 0px";
-	banner.style.paddingLeft = "10px";
-	banner.style.backgroundColor = "#DAE4EB";
-	banner.style.lineHeight = "2.5em";
-	banner.style.fontSize = "0.8em";
-
-	var grid = document.getElementById("grid");
-	grid.style.height = "100%";
-	grid.style.display = "flex";
-	grid.style.border = "0px";
-	grid.scrolling = "no";
-	grid.onload = onGridLoad;
-	grid.src="vip_grid.html";
-}
-
-function onGridLoad()
-{
 	var ivipgrid = document.getElementById("grid").contentWindow.IVipGrid;
 
-	//ivipgrid.layout.col_count = 10;
+	ivipgrid.layout.col_count = 8;
 	//ivipgrid.layout.font_scale = 1;
+	//ivipgrid.layout.col_offset = false;
+	//ivipgrid.layout.highlight_weekends = false;
+
 	ivipgrid.create();
 }
