36790af54be1ead6198bf4c4bff5ebff27ca3c13
ct <ct6787@gmail.com>
2017-02-20 22:54:12 +0000

    dev

--- vip_lib.js
+++ vip_lib.js
@@ -572,7 +572,6 @@ VipCell.prototype.updateEventLayout = function()
 		vipevt = vipevt.Next();
 	}
 
-/*
 	if (vip.events.title.show)
 	{
 		while (tot_width > this.vipevts.div.offsetWidth)
@@ -595,7 +594,6 @@ VipCell.prototype.updateEventLayout = function()
 			tot_width += longest.div.offsetWidth;
 		}
 	}
-*/
 
 	var x_off = 0;
 
